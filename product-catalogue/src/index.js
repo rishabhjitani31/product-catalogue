bdc34abcee50e898d3090d257544ff680d63b8ab
rishabhjitani31 <you@example.com>
2018-09-02 21:30:11 +0530

    removed the extra and unused folder of product-catalogue

--- product-catalogue/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import AppLayout from './modules/productCatalogue/layout';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<AppLayout />, document.getElementById('root'));
-registerServiceWorker();
